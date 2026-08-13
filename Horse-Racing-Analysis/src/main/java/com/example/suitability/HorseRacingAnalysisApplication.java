d21cd0cfbb2bc20b0b78f51ae316f7069bc400fa
yuutabata-GRW <yuutabata.grouthway@gmail.com>
2026-08-13 10:44:51 +0900

    環境構築

---
+++ Horse-Racing-Analysis/src/main/java/com/example/suitability/HorseRacingAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.example.suitability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorseRacingAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorseRacingAnalysisApplication.class, args);
+	}
+
+}
---
+++ Horse-Racing-Analysis/src/test/java/com/example/suitability/HorseRacingAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.suitability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HorseRacingAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
